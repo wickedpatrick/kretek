fa75d01dcd12587f304bca31305ca1b2448fc804
Patryk Kozak <pkozak@gamesture.com>
2019-07-26 11:28:31 +0200

    smieci

--- logic.js
+++ logic.js
@@ -1,1 +1,33 @@
-var xd = "dupa";
+let xd = "dupa";
+
+let initLogic = function ()
+{
+    Window.loadLevel = loadLevel; //put function to global scope
+};
+
+let loadLevel = function(levelNo)
+{
+    // alert('huj w dupe');
+
+    if (levelNo.toString().length === 1) {
+        levelNo = '0' + levelNo;
+    }
+
+    let fileName = 'level' + levelNo + '.txt';
+
+    console.log("loadig from string: " + fileName);
+
+    let data = [];
+    jQuery.get(fileName, function(data) {
+
+    });
+
+    console.log(data);
+
+    // let client = new XMLHttpRequest();
+    // client.open('GET', '/level01.txt');
+    // client.onreadystatechange = function() {
+    //     alert(client.responseText);
+    // }
+    // client.send();
+};
