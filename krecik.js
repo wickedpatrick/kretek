bb2f777c4201bb7624a5fe648ad22cf20dea1de9
Adam Kociolek <akociolek@gamesture.com>
2019-07-26 10:45:00 +0200

    myinit

---
+++ krecik.js
@@ -0,0 +1,16 @@
+class Kretek {
+	
+	constructor(x, y) {
+		this.posX = x;
+		this.posY = y;
+		this.domObj = document.querySelector('#krecik');
+		//console.log(this.domObj);
+		//this.domObj.style.width = '50px';
+		//this.domObj.style.height = '50px';
+		//this.domObj.style.backgroundColor = 'pink';
+		
+	}
+}
+
+let Kreciu = new Kretek(0,0);
+
